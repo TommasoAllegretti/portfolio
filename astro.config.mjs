3d79c5285418d253976ee2a8c8b6dc1872e12bed
Tommaso Allegretti <allegretti.tommaso@gmail.com>
2025-05-14 23:14:17 +0200

    added sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  site: "https://tommasoallegretti.com",
+  integrations: [tailwind(), react(), sitemap()],
   vite: {
     resolve: {
       alias: {
